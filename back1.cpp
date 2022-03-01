19e350e124ba4c23fa21c985d6161dfe4d9e0425
Ashir Vp <54493280+ashirvp@users.noreply.github.com>
2022-03-01 07:21:59 +0530

    Initial commit

---
+++ back1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int age=20;
+    int mark=55;
+    int mark1=65;
+
+    cout<<"The mark is : "<<mark<<endl<<" age is : "<<age<<endl<<" next mark is: "<<mark1;
+    return 0;
+}
---
+++ mark.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    int mark1,mark2,mark3;
+
+    cout<<"Enter mark 1: ";cin>>mark1;
+    cout<<"Enter mark 2: ";cin>>mark2;
+    cout<<"Enter mark 3: ";cin>>mark3;
+
+    int total_mark=mark1+mark2+mark3;
+    cout<<"total mark in 3 sub is: "<<total_mark;
+
+
+    
+    return 0;
+
+}
