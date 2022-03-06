66e4735e979b408c9ab6d09b1da66c78877d27af
Ashir Vp <54493280+ashirvp@users.noreply.github.com>
2022-03-06 14:35:16 +0530

    a

--- doub.cpp
+++ doub.cpp
@@ -1,24 +1,26 @@
 #include <iostream>
 #include <iomanip>
-#include <bits/stdc++.h>
+//#include <bits/stdc++.h>
 using namespace std;
 
 int main(){
-
-double x,y,z;
-x=2.50254557;
-y=-2.3564;
-z=1.2;
-
-cout<<setw(5)<<x<<endl;
-cout<<fixed<<setprecision(5)<<x<<endl;
-cout<<floor(y)<<endl;
-cout<<ceil(x)<<endl;
-cout<<trunc(z)<<endl;
-cout<<round(x)<<endl;
-
-
-
+cout<<"enter the mark";
+int a;
+cin>>a;
+if(a>90){
+    cout<<"You got S grade"<<endl;
+}else if(a>80){
+    cout<<"You got A grade"<<endl;
+}else if(a>70){
+    cout<<"You got B grade"<<endl;
+}else if(a>60){
+    cout<<"You got C grade"<<endl;
+}else if(a>50){
+    cout<<"You got D grade"<<endl;
+}else{
+    cout<<"You got F grade:"<<endl;
+    cout<<"Sorry..failed"<<endl;
+}
     return 0;
 }
 
